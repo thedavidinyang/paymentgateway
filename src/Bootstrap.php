56e049e85ebe8ec24119fdd4e9a2c4c84ee0df8d
David Inyang <davidinyang01@gmail.com>
2024-05-30 11:31:34 +0100

    update

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -4,4 +4,10 @@ namespace thedavidinyang\paymentGateway;
 Class Bootstrap
 {
 
+    private $monnifyConfig;
+    private $paystackConfig;
+    private $flutterwaveConfig;
+    private $config;
+
+
 }
