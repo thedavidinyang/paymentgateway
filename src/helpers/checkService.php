710d6e451a5a8a2ea65edd07c198583ec501abf5
David Inyang <davidinyang01@gmail.com>
2024-06-12 10:05:09 +0100

    upDATE

--- src/helpers/checkService.php
+++ src/helpers/checkService.php
@@ -9,37 +9,20 @@ trait checkService
 {
 
 /**
- * Compare provided config with expected config.
+ * Check if the provided service is in the list of expected services.
  *
- * @param array $providedConfig The first array to compare.
- * @param array $expectedConfig The second array to compare.
- * @return bool Returns true if the arrays have the same keys and the same count, false otherwise.
+ * @param string $providedService The service to check.
+ * @param array $expectedService The list of expected services.
+ * @return bool True if the provided service is in the list of expected services, false otherwise.
+ * @throws Exception if the provided service is not in the list of expected services.
  */
-    public function checker(array $providedConfig, array $expectedConfig) : bool
-    {
-        try {
-
-            // Get the keys of both arrays
-            $keys1 = array_keys($providedConfig);
-            $keys2 = array_keys($expectedConfig);
-
-            // Check if both arrays have the same count of elements
-            if (count($providedConfig) !== count($expectedConfig)) {
-                throw new Exception('Expected config parameters not met');
-
-            }
-
-            // Check if both arrays have the same keys
-            if (array_diff($keys1, $keys2) || array_diff($keys2, $keys1)) {
-                throw new Exception('Expected config parameters not met');
-            }
-
-            return true;
-
-        } catch (\Throwable $e) {
-            // Optionally log the error or perform other actions before throwing
-            throw new Exception('Configuration error: ' . $e->getMessage(), 0, $e);
-        }
+public function serviceCheck(string $providedService, array $expectedService): bool
+{
+    if (in_array($providedService, $expectedService, true)) {
+        return true;
+    } else {
+        throw new Exception('The chosen provider is not supported.');
     }
+}
 
 }
